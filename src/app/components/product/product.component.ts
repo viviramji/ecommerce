e1e758062fe11c6254dcfa45d9441ba26bf38f77
Victor Victor RJ <viviramji@gmail.com>
2020-06-30 20:35:54 -0500

    Component life-cycle

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  Output, 
+  EventEmitter, 
+  OnChanges, 
+  SimpleChanges } from '@angular/core';
+  
 import { Product } from 'src/app/models/product.model';
 
 /**Represents the metadata of the component */
@@ -8,13 +16,38 @@ import { Product } from 'src/app/models/product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnInit {
-  ngOnInit(): void {}
+export class ProductComponent implements OnInit, OnChanges {
+  
 
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
-  constructor() {}
+  constructor() {
+    console.log('1. constructor')
+  }
+  
+  ngOnInit(): void {
+    console.log(' 3. on init')
+    // It's a good practice to call data services from this method
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('2. ngOnChanges');
+    console.log(changes);
+  }
+
+  /**
+   * ngOnCheck() {
+   *  this is a way to check manually changes instead ngOnChanges angular 
+   *  verify the changes
+   * }
+   *  ngOnDestroy() {
+   *   clean memory process when a component is delete from the UI
+   * }
+   * 
+   */
+
+  
 
   addCart(): void {
     console.log('hello from component child');
