f7b50f419a9bafb4a81ec4b43b13c04a12c8674a
Victor Victor RJ <viviramji@gmail.com>
2020-07-02 01:54:08 -0500

    shared module and core module... no comentarios </3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CartComponent } from './components/cart/cart.component';
-import { ExponentialPipe } from './pipes/exponential.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
+
 import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { BaseComponent } from './layouts/base/base.component';
+
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     CartComponent,
-    ExponentialPipe,
-    HighlightDirective,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
-    HeaderComponent,
-    FooterComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
     BaseComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    SharedModule,
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -5,9 +5,10 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from './../../shared/shared.module';
 
 @NgModule({
   declarations: [BannerComponent, HomeComponent],
-  imports: [HomeRoutingModule, CommonModule],
+  imports: [HomeRoutingModule, CommonModule, SharedModule],
 })
 export class HomeModule {}
